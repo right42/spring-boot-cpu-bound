7dcfe4d1d6633b1b973ee1ea9019460d277defad
right42 <chjm452@naver.com>
2021-04-02 19:23:19 +0900

    spring boot cpu bound application init

---
+++ src/main/java/me/right42/cpubound/demo/CpuBoundAppApplication.java
@@ -0,0 +1,13 @@
+package me.right42.cpubound.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuBoundAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpuBoundAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/right42/cpubound/demo/HashController.java
@@ -0,0 +1,39 @@
+package me.right42.cpubound.demo;
+
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+public class HashController {
+
+
+    @GetMapping("/hash/{input}")
+    public String getHash(@PathVariable String input) {
+        for (int i = 0; i < 100000; i++) {
+            input = getMd5Digest(input);
+        }
+        return input;
+    }
+
+    private String getMd5Digest(String input) {
+        byte[] digest = DigestUtils.md5Digest(input.getBytes(StandardCharsets.UTF_8));
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < digest.length; i++) {
+            String hex = Integer.toHexString(0xff & digest[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+}
